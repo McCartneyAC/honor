d1c3f4213e50952248225afc4bb63c5cebaa74aa
McCartneyAC <acm9q@virginia.edu>
2018-06-08 15:18:56 -0400

    created demographic mosaic plot

--- graph_functions.R
+++ graph_functions.R
@@ -1,7 +1,34 @@
 # graph functions
 
-eq_mosaic<-function(){
-  
+uvapal<-c("#E57200", #orange
+          "#232D4B", #navy
+          "#007681",
+          "#F2CD00",
+          "#692A7E", 
+          "#84BD00",
+          "#A5ACAF", #gray
+          "#5C7F92",
+          "#857363",
+          "#CAC0B6"  #ten total colors
+          )
+
+eq_mosaic<-function(data){
+  # REQUIRES PRECISE VARIABLE NAMES
+  # this will be a problem when we implement this later.
+  data <- melt(data)
+  require(ggplot2)
+  require(ggmosaic)
+  require(ggthemes)
+  p<-ggplot(data=data) + geom_mosaic(aes(weight=data$value, x = product(data$variable), fill = data$group))+
+    scale_fill_manual(values=uvapal)+
+     labs(
+       title="Demographic Percentages",
+       x = "",
+       y = "Percent by Demographic",
+       color = "Demographic Groups"
+     ) + 
+     theme_bw()
+  return(p)
 }
 
 eq_survival<-function(){
