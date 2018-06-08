40e3e68c4a74cd4a372917390fd18da3f50a05df
McCartneyAC <acm9q@virginia.edu>
2018-06-08 14:37:13 -0400

    update EI

--- metrics.R
+++ metrics.R
@@ -3,9 +3,9 @@
 RCDI<-function(groups,pop,sample){
   # requires data input to be a list of a # of students for each race
   # TODO: Rewrite as a tidy version. 
-  pctgift<-pop / sum(pop)
-  pcttot <-sample / sum(sample)
-  z <- sample-pop
+  pcttot<-pop / sum(pop)
+  pctgift <-sample / sum(sample)
+  z <- pop-sample
   pctnon<-z / sum(z)
   rcdi <- ((pctgift - pctnon ) / (pctnon)) * 100
   rcdi <- round(rcdi, digits =2)
@@ -14,12 +14,17 @@ RCDI<-function(groups,pop,sample){
   return(mat)
 }
 
-EI<-function(x,y){
-  pctgift<-x / sum(x)
-  pcttot <-y / sum(y)
-  z <- y-x
+EI<-function(groups,pop,sample){
+  # requires data input to be a list of a # of students for each race
+  # TODO: Rewrite as a tidy version to allow for piping over data frame restructuring
+  pcttot<-pop / sum(pop)
+  pctgift <-sample / sum(sample)
+  z <- pop-sample
   pctnon<-(z / sum(z))*100
   part1<- pctnon*0.20
   part2<- pctnon-part1
-  return(part2)
+  Number_Needed<-round(part2, digits = 0)
+  grps <- groups
+  mat <- as.data.frame(cbind(grps, Number_Needed))
+  return(mat)
 }
