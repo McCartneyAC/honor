7cdc1335a18f0ab81b9fd11b625e2cbac0297730
McCartneyAC <acm9q@virginia.edu>
2018-06-11 14:50:33 -0400

    Create global.R

---
+++ global.R
@@ -0,0 +1,8 @@
+library(shiny)
+library(shinydashboard)
+library(shinythemes)
+library(readxl)
+library(ggalluvial)
+library(tidyverse) # TIDYVERSE ALWAYS COMES LAST.
+
+colors<-c("blue", "black", "purple", "green", "red", "yellow") 
