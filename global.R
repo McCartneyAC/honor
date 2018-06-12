561430abfe5c9405c5df02f75aa1fab8defa3f13
McCartneyAC <acm9q@virginia.edu>
2018-06-12 10:37:05 -0400

    Update global.R

--- global.R
+++ global.R
@@ -6,3 +6,15 @@ library(ggalluvial)
 library(tidyverse) # TIDYVERSE ALWAYS COMES LAST.
 
 colors<-c("blue", "black", "purple", "green", "red", "yellow") 
+
+uvapal<-c("#E57200", #orange
+          "#232D4B", #navy
+          "#007681",
+          "#F2CD00",
+          "#692A7E", 
+          "#84BD00",
+          "#A5ACAF", #gray
+          "#5C7F92",
+          "#857363",
+          "#CAC0B6"  #ten total colors
+          )
