2106b10ca7b08ce0a4c2edb68f418d08f85aa19c
McCartneyAC <acm9q@virginia.edu>
2018-09-19 17:31:42 -0400

    Update global.R

--- global.R
+++ global.R
@@ -7,10 +7,12 @@ library(ggalluvial)
 library(ggmosaic)
 library(ggthemes)
 library(lubridate)
-library(dplyr) # subbing dplyr for whole tidyverse to speed up upload times to shinyapps.io
+library(anicon)
+library(wired)
+library(dplyr) 
 
 colors<-c("blue", "black", "purple", "green", "red", "yellow") # shinyDashboard available colors.
-                                                               # app will choose one of these at random to display as header bar color.
+# app will choose one of these at random to display as header bar color.
 
 uvapal<-c("#E57200", #orange
           "#232D4B", #navy
@@ -22,4 +24,4 @@ uvapal<-c("#E57200", #orange
           "#5C7F92",
           "#857363",
           "#CAC0B6"  #ten total colors
-          )
+)
